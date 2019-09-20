7da7fe414cee49df4c8f558bd39d91c03eb817a6
grxself <grxself@gmail.com>
2019-09-20 17:41:18 +0800

    SET-036: When call GetRoom, session set room id.

--- ScrumEstimationTool.Api/Controllers/RoomController.cs
+++ ScrumEstimationTool.Api/Controllers/RoomController.cs
@@ -19,6 +19,8 @@ namespace ScrumEstimationTool.Api.Controllers
                 return NotFound();
             }
 
+            HttpContext.Session.SetInt32(KeyRoomId, room.Id);
+
             return room;
         }
 
